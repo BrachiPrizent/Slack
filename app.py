54b0795e84a026228e104e7dca29c589e270ee97
B. Prizent <brachip@skyvar.co.il>
2025-12-03 11:02:12 +0200

    :tada: Add all project

---
+++ app.py
@@ -0,0 +1,78 @@
+import getopt
+import os
+import requests
+import sys
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+
+load_dotenv()
+
+def main(argv):
+    message = ' '
+    try: 
+        opts, args = getopt.getopt(argv, "hm:", ["message="])
+
+    except getopt.GetoptError:
+        print('slack.py -m <message>')
+        sys.exit(2)
+
+    if len(opts) == 0:
+        message = 'HELLO, WORLD!'
+    for opt, arg in opts:
+        if opt == '-h':
+            print('slack.py -m <message>')
+            sys.exit()
+        elif opt in ("-m", "--message"):
+            message = arg
+
+    send_slack_message(message)
+    slack_token = os.getenv("SLACK_BOT_TOKEN")
+    client = WebClient(token=slack_token)
+    get_list_of_channels(client)
+    get_list_of_users(client)
+
+def get_list_of_users(client):
+    try:
+        response = client.users_list()
+        members = response["members"]
+        user_list = []
+        for member in members:
+            if not member['is_bot'] and not member['deleted']:
+                user_list.append({
+                    "name": member.get("real_name", member.get("name")),
+                    "id": member["id"],
+                    "email": member["profile"].get("email")
+                })
+        print("Connected User List:")
+        for user in user_list:
+            print(f"- Name: {user['name']}, ID: {user['id']}, Email: {user['email']}")
+
+    except Exception as e:
+        print(f"Error fetching users: {e}")
+
+def get_list_of_channels(client):
+    try:
+        response = client.conversations_list(types="public_channel")
+        channels = response["channels"]
+        channel_list = []
+        for channel in channels:
+            channel_list.append({
+                "name": channel["name"],
+                "id": channel["id"]
+            })
+        print("\nConnected Channel List:")
+        for channel in channel_list:
+            print(f"- Name: {channel['name']}, ID: {channel['id']}")
+
+    except Exception as e:
+        print(f"Error fetching channels: {e}")
+
+def send_slack_message(message):
+    payload = {"text": message}
+    SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+    response = requests.post(SLACK_WEBHOOK_URL, json = payload, verify=False)
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
